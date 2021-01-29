445946602484c1bada113c68d0ea0b2b267372e7
Katherine Castro <katherine132000@hotmail.com>
2021-01-29 13:34:32 -0400

    Agregar archivos de proyecto.

---
+++ Numeros.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Numeros
+{
+	public class Numero
+	{
+		private int[,] tabla;
+		public void CargarTabla()
+        {
+			tabla = new int[4, 5];
+			Console.WriteLine("Ingresa los componentes: ");
+			for (int j=0;j< 4; j++)
+            {
+				for (int k = 0; k < 5; k++)
+                {					
+					tabla[j, k] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+		public void ImprimirTabla()
+		{
+			Console.WriteLine("Los componentes son ");
+			for (int j = 0; j < 4; j++)
+			{
+				for (int k = 0; k < 5; k++)
+				{
+					Console.Write("\n");
+					Console.Write(tabla[j,k]+" ");
+				}
+			}
+			Console.ReadKey();
+		}
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Numeros;
+namespace eval02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = "Hola!";
+            Numero obNum = new Numero();
+            Console.WriteLine(cadena);
+            obNum.CargarTabla();
+            obNum.ImprimirTabla();
+
+        }
+    }
+}
